70b97cfd08069a8d6b5b412b2e0dd2604ae34a04
cs344 <61498658+cs344@users.noreply.github.com>
2020-12-30 12:33:09 +0000

    Module 1 Exploration 5 Variables and Data Types
    Ask the user for two positive integers.
    Print all the multiples of the first integer between 1 and the second integer

---
+++ module1/1_5_multiplier/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* Ask the user for two positive integers.
+* Print all the multiples of the first integer between 1 and the second integer
+*/
+int main(void) {
+
+  /* Declare variables and their types*/
+  int boundary;
+  int denominator;
+
+  printf("Enter a positive integer whose multiples you want printed: ");
+  scanf("%d", &denominator);
+  printf("Enter another positive integer up to which you want to see multiples printed: ");
+  scanf("%d", &boundary);
+  printf("Here are all the multiples of %d between 1 & %d\n", denominator, boundary);
+  for(int i = 1; i <= boundary; i++){
+    if(i % denominator == 0){
+      printf("%d ", i);
+    }
+  }
+  printf("\n");
+  return 0;
+}
