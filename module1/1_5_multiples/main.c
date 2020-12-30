f51e4caf76fe6e7bf84e1efcaf71b6dc705a1df3
cs344 <61498658+cs344@users.noreply.github.com>
2020-12-30 18:59:53 +0000

    Update main.c

--- module1/1_5_multiples/main.c
+++ module1/1_5_multiples/main.c
@@ -4,22 +4,16 @@
 * Ask the user for two positive integers.
 * Print all the multiples of the first integer between 1 and the second integer
 */
-int main(void) {
 
-  /* Declare variables and their types*/
-  int denominator;
-  int boundary;
+int main(void)
+{
+    int denominator;
+    int boundary;
 
-  printf("Enter a positive integer whose multiples you want printed: ");
-  scanf("%d", &denominator);
-  printf("Enter another positive integer up to which you want to see multiples printed: ");
-  scanf("%d", &boundary);
-  printf("Here are all the multiples of %d between 1 & %d\n", denominator, boundary);
-  for(int i = 1; i <= boundary; i++){
-    if(i % denominator == 0){
-      printf("%d ", i);
-    }
-  }
-  printf("\n");
-  return 0;
+    printf("Enter a positive integer whose multiples you want printed: ");
+    scanf("%d", &denominator);
+    printf("Enter another positive integer up to which you want to see multiples printed: ");
+    scanf("%d", &boundary);
+    printf("You entered %d for denominator and %d for boundary\n", denominator, boundary);
+    return 0;
 }
