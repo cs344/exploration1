5c29547f07f1b87e1f44056367638b61596f5cee
cs344 <61498658+cs344@users.noreply.github.com>
2020-12-30 17:20:38 +0000

    Update main.c
    Add link to man page for write

--- module1/1_4_hello_world_put/main.c
+++ module1/1_4_hello_world_put/main.c
@@ -1,5 +1,8 @@
 #include <unistd.h>
 
+// Man page for write system call
+// https://www.man7.org/linux/man-pages/man2/write.2.html
+
 int main(void) {
   write(1, "Hello World\n", 12);
   return 0;
